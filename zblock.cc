c259ed06f19903099cfcdfab5688c6e5e3ffdf50
EdwinYi-JanYang <51840314+EdwinYi-JanYang@users.noreply.github.com>
2021-08-05 18:12:57 -0400

    Update zblock.cc

--- zblock.cc
+++ zblock.cc
@@ -2,8 +2,10 @@
 
 ZBlock::ZBlock(std::vector<std::vector<Cell *>> board, int level)
 {
+	vertical = false;
 	SetType('Z');
 	SetLevel(level);
+	SetPosition(14, 0);
 	board[15][0]->SetType('Z');
 	board[15][0]->SetOwner(this);
 	board[15][1]->SetType('Z');
@@ -20,4 +22,33 @@ ZBlock::ZBlock(std::vector<std::vector<Cell *>> board, int level)
 
 void ZBlock::Rotate(char direction, std::vector<std::vector<Cell *>> board) {
 	std::cout << "zblockrotate " << direction << std::endl;
+	std::vector<Cell *> newCells;
+	int size = cells.size();
+	for (int i = 0; i < size; i++) {
+		cells[i]->SetType('.');
+		cells[i]->SetOwner(nullptr);
+		if (vertical) {
+			newCells.push_back(board[cornerRow][cornerCol + i]);
+		} else {
+			newCells.push_back(board[cornerRow + i][cornerCol]);
+		}
+	}
+	if (vertical) {
+		newCells.push_back(board[cornerRow+1][cornerCol]);
+		newCells.push_back(board[cornerRow+1][cornerCol+1]);
+		newCells.push_back(board[cornerRow][cornerCol+1]);
+		newCells.push_back(board[cornerRow][cornerCol+2]);
+	}
+	else {
+		newCells.push_back(board[cornerRow+2][cornerCol]);
+		newCells.push_back(board[cornerRow+1][cornerCol]);
+		newCells.push_back(board[cornerRow+1][cornerCol+1]);
+		newCells.push_back(board[cornerRow][cornerCol+1]);
+	}
+	for (int i = 0; i < newCells.size(); i++) {
+		newCells[i]->SetType('Z');
+		newCells[i]->SetOwner(this);
+	}
+	cells = newCells;
+	vertical = !vertical;
 }
