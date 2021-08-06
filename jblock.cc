cabcbeccc95cd3f44b6cffd1cb2d2f3ebf4edbf8
EdwinYi-JanYang <51840314+EdwinYi-JanYang@users.noreply.github.com>
2021-08-05 20:11:53 -0400

    Update jblock.cc

--- jblock.cc
+++ jblock.cc
@@ -2,8 +2,10 @@
 
 JBlock::JBlock(std::vector<std::vector<Cell *>> board, int level)
 {
+	position = 0;
 	SetType('J');
 	SetLevel(level);
+	SetPosition(14, 0);
 	board[15][0]->SetType('J');
 	board[15][0]->SetOwner(this);
 	board[14][0]->SetType('J');
@@ -22,4 +24,49 @@ void JBlock::Rotate(char direction, std::vector<std::vector<Cell *>> board) {
 	std::cout << "jblockrotate " << direction << std::endl;
 	std::vector<Cell *> newCells;
 	int size = cells.size();
+	if (direction == counterclockwise) {
+		position++;
+	}
+	else {
+		position--;
+	}
+	position = position%4;
+	if (position == 3) {
+		newCells.push_back(board[cornerRow][cornerCol]);
+		newCells.push_back(board[cornerRow][cornerCol+1]);
+		newCells.push_back(board[cornerRow+1][cornerCol+1]);
+		newCells.push_back(board[cornerRow+2][cornerCol+1]);
+	}
+	if (position == 2) {
+		newCells.push_back(board[cornerRow+1][cornerCol]);
+		newCells.push_back(board[cornerRow+1][cornerCol+1]);
+		newCells.push_back(board[cornerRow+1][cornerCol+2]);
+		newCells.push_back(board[cornerRow][cornerCol+2]);
+	}
+	if (position == 1) {
+		newCells.push_back(board[cornerRow][cornerCol]);
+		newCells.push_back(board[cornerRow+1][cornerCol]);
+		newCells.push_back(board[cornerRow+2][cornerCol]);
+		newCells.push_back(board[cornerRow+2][cornerCol+1]);
+	}
+	else {
+		newCells.push_back(board[cornerRow][cornerCol]);
+		newCells.push_back(board[cornerRow+1][cornerCol]);
+		newCells.push_back(board[cornerRow][cornerCol+1]);
+		newCells.push_back(board[cornerRow][cornerCol+2]);
+	}
+	for (int i = 0; i < newCells.size(); i++) {
+		if (newCells[i]->GetOwner() != nullptr && newCells[i]->GetOwner() != this) {
+			return;
+		}
+	}
+	for (int i = 0; i < size; i++) {
+		cells[i]->SetType('.');
+		cells[i]->SetOwner(nullptr);
+	}
+	for (int i = 0; i < newCells.size(); i++) {
+		newCells[i]->SetType('Z');
+		newCells[i]->SetOwner(this);
+	}
+	cells = newCells;
 }
