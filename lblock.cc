32ffea1f3972cff27f7d95a1b525c0e669ab6fe9
EdwinYi-JanYang <51840314+EdwinYi-JanYang@users.noreply.github.com>
2021-08-05 20:15:06 -0400

    Update lblock.cc

--- lblock.cc
+++ lblock.cc
@@ -2,6 +2,7 @@
 
 LBlock::LBlock(std::vector<std::vector<Cell *>> board, int level)
 {
+	position = 0;
 	SetType('L');
 	SetLevel(level);
 	board[14][0]->SetType('L');
@@ -20,4 +21,51 @@ LBlock::LBlock(std::vector<std::vector<Cell *>> board, int level)
 
 void LBlock::Rotate(char direction, std::vector<std::vector<Cell *>> board) {
 	std::cout << "lblockrotate " << direction << std::endl;
+	std::vector<Cell *> newCells;
+	int size = cells.size();
+	if (direction == counterclockwise) {
+		position++;
+	}
+	else {
+		position--;
+	}
+	position = position%4;
+	if (position == 3) {
+		newCells.push_back(board[cornerRow][cornerCol]);
+		newCells.push_back(board[cornerRow][cornerCol+1]);
+		newCells.push_back(board[cornerRow][cornerCol+2]);
+		newCells.push_back(board[cornerRow+1][cornerCol+2]);
+	}
+	if (position == 2) {
+		newCells.push_back(board[cornerRow+2][cornerCol]);
+		newCells.push_back(board[cornerRow+2][cornerCol+1]);
+		newCells.push_back(board[cornerRow+1][cornerCol+1]);
+		newCells.push_back(board[cornerRow][cornerCol+1]);
+	}
+	if (position == 1) {
+		newCells.push_back(board[cornerRow][cornerCol]);
+		newCells.push_back(board[cornerRow+1][cornerCol]);
+		newCells.push_back(board[cornerRow+][cornerCol+1]);
+		newCells.push_back(board[cornerRow+1][cornerCol+2]);
+	}
+	else {
+		newCells.push_back(board[cornerRow][cornerCol]);
+		newCells.push_back(board[cornerRow+1][cornerCol]);
+		newCells.push_back(board[cornerRow+2][cornerCol]);
+		newCells.push_back(board[cornerRow][cornerCol+1]);
+	}
+	for (int i = 0; i < newCells.size(); i++) {
+		if (newCells[i]->GetOwner() != nullptr && newCells[i]->GetOwner() != this) {
+			return;
+		}
+	}
+	for (int i = 0; i < size; i++) {
+		cells[i]->SetType('.');
+		cells[i]->SetOwner(nullptr);
+	}
+	for (int i = 0; i < newCells.size(); i++) {
+		newCells[i]->SetType('L');
+		newCells[i]->SetOwner(this);
+	}
+	cells = newCells;
 }
