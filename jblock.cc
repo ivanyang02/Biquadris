fe544a90a7d3798efd7172d40206b67fe390859d
a33tsang <a33tsang@student.cs.uwaterloo.ca>
2021-08-06 00:07:19 -0400

    fix jblock rotation

--- jblock.cc
+++ jblock.cc
@@ -24,46 +24,53 @@ void JBlock::Rotate(char direction, std::vector<std::vector<Cell *>> board) {
 	std::cout << "jblockrotate " << direction << std::endl;
 	std::vector<Cell *> newCells;
 	int size = cells.size();
-	if (direction == 'l') {
+
+	if (position % 2 == 1) {
+	    if (cornerCol + 2 >= 11) {
+			return;
+		}
+	}
+	if (direction == 'r') {
 		position++;
 	}
 	else {
 		position--;
 	}
+
 	position = position%4;
 	if (position == 3) {
 		newCells.push_back(board[cornerRow][cornerCol]);
 		newCells.push_back(board[cornerRow][cornerCol+1]);
 		newCells.push_back(board[cornerRow+1][cornerCol+1]);
 		newCells.push_back(board[cornerRow+2][cornerCol+1]);
-	}
-	if (position == 2) {
+	} else if (position == 2) {
 		newCells.push_back(board[cornerRow+1][cornerCol]);
 		newCells.push_back(board[cornerRow+1][cornerCol+1]);
 		newCells.push_back(board[cornerRow+1][cornerCol+2]);
 		newCells.push_back(board[cornerRow][cornerCol+2]);
-	}
-	if (position == 1) {
+	} else if (position == 1) {
 		newCells.push_back(board[cornerRow][cornerCol]);
 		newCells.push_back(board[cornerRow+1][cornerCol]);
 		newCells.push_back(board[cornerRow+2][cornerCol]);
 		newCells.push_back(board[cornerRow+2][cornerCol+1]);
-	}
-	else {
+	} else {
 		newCells.push_back(board[cornerRow][cornerCol]);
 		newCells.push_back(board[cornerRow+1][cornerCol]);
 		newCells.push_back(board[cornerRow][cornerCol+1]);
 		newCells.push_back(board[cornerRow][cornerCol+2]);
 	}
+
 	for (int i = 0; i < newCells.size(); i++) {
 		if (newCells[i]->GetOwner() != nullptr && newCells[i]->GetOwner() != this) {
 			return;
 		}
 	}
+
 	for (int i = 0; i < size; i++) {
 		cells[i]->SetType('.');
 		cells[i]->SetOwner(nullptr);
 	}
+
 	for (int i = 0; i < newCells.size(); i++) {
 		newCells[i]->SetType('J');
 		newCells[i]->SetOwner(this);
