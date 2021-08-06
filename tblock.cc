c0006423bfbec93fb6b6dc844477ffff07e3da52
EdwinYi-JanYang <51840314+EdwinYi-JanYang@users.noreply.github.com>
2021-08-05 20:30:16 -0400

    Update tblock.cc

--- tblock.cc
+++ tblock.cc
@@ -20,4 +20,51 @@ TBlock::TBlock(std::vector<std::vector<Cell *>> board, int level)
 
 void TBlock::Rotate(char direction, std::vector<std::vector<Cell *>> board) {
 	std::cout << "tblockrotate " << direction << std::endl;
+	std::vector<Cell *> newCells;
+	int size = cells.size();
+	if (direction == counterclockwise) {
+		position++;
+	}
+	else {
+		position--;
+	}
+	position = position%4;
+	if (position == 3) {
+		newCells.push_back(board[cornerRow+1][cornerCol]);
+		newCells.push_back(board[cornerRow+1][cornerCol+1]);
+		newCells.push_back(board[cornerRow+2][cornerCol+1]);
+		newCells.push_back(board[cornerRow][cornerCol+1]);
+	}
+	if (position == 2) {
+		newCells.push_back(board[cornerRow][cornerCol]);
+		newCells.push_back(board[cornerRow][cornerCol+1]);
+		newCells.push_back(board[cornerRow][cornerCol+2]);
+		newCells.push_back(board[cornerRow+1][cornerCol+1]);
+	}
+	if (position == 1) {
+		newCells.push_back(board[cornerRow][cornerCol]);
+		newCells.push_back(board[cornerRow+1][cornerCol]);
+		newCells.push_back(board[cornerRow+2][cornerCol]);
+		newCells.push_back(board[cornerRow+1][cornerCol+1]);
+	}
+	else {
+		newCells.push_back(board[cornerRow+1][cornerCol]);
+		newCells.push_back(board[cornerRow+1][cornerCol+1]);
+		newCells.push_back(board[cornerRow+1][cornerCol+2]);
+		newCells.push_back(board[cornerRow][cornerCol+1]);
+	}
+	for (int i = 0; i < newCells.size(); i++) {
+		if (newCells[i]->GetOwner() != nullptr && newCells[i]->GetOwner() != this) {
+			return;
+		}
+	}
+	for (int i = 0; i < size; i++) {
+		cells[i]->SetType('.');
+		cells[i]->SetOwner(nullptr);
+	}
+	for (int i = 0; i < newCells.size(); i++) {
+		newCells[i]->SetType('T');
+		newCells[i]->SetOwner(this);
+	}
+	cells = newCells;
 }
