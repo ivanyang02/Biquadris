0decf735dd09a3586f4141b4751420db0bfbb02b
EdwinYi-JanYang <51840314+EdwinYi-JanYang@users.noreply.github.com>
2021-08-12 14:04:20 -0400

    Added documenting

--- block.h
+++ block.h
@@ -15,18 +15,21 @@ class Block {
 		std::vector<Cell *> cells;
 	public:
 		//virtual Block(char type, Cell *board[18][11]) = 0;
-		virtual ~Block();
-		virtual int GetLevel();
-		virtual int GetCoRow();
-		virtual int GetCellsCount();
-		virtual void SetLevel(int level);
-		virtual void SetType(char type);
-		virtual void SetPosition(int row, int col);
-		virtual void AddCell(Cell *cell);
-		virtual void RemoveCell(Cell *cell);
+		virtual ~Block(); //virtual distructor for abstract class
+		virtual int GetLevel(); //returns the level field of the object instance
+		virtual int GetCoRow(); //returns the cornerRow of the object instance
+		virtual int GetCellsCount(); //returns the number of cells in vector cells
+		virtual void SetLevel(int level); //sets the level field
+		virtual void SetType(char type); //sets the type field
+		virtual void SetPosition(int row, int col); //sets both the cornerRow and cornerCol fields
+		virtual void AddCell(Cell *cell); //adds the cell to the vector of cells
+		virtual void RemoveCell(Cell *cell); //removes the specified cell from the vector of cells if present
 		virtual bool Move(char direction, std::vector<std::vector<Cell *>> board);
+		//shifts the block on position right, left, or down depending on the direction parameter on the given board, if it cannot move it does nothing
 		virtual void Rotate(char direction, std::vector<std::vector<Cell *>> board) = 0;
+		//rotates the block clockwise or counterclockwise depending on the direction parameter on the given board, if it cannot move it does nothing
 		virtual void Drop(std::vector<std::vector<Cell *>> board);
+		//moves the block down until it cannot move
 };
 
 #endif
