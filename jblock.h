7d875bafe98ea25d9b9cf3931a8b88623947dc00
EdwinYi-JanYang <51840314+EdwinYi-JanYang@users.noreply.github.com>
2021-08-12 16:21:33 -0400

    Added documentation

--- jblock.h
+++ jblock.h
@@ -7,10 +7,11 @@
 #include "block.h"
 
 class JBlock : public Block {
-	int position;
+	int position; //records the 4 possible positions
 	public:
-		JBlock(std::vector<std::vector<Cell *>> board, int level, int row, int col);
+		JBlock(std::vector<std::vector<Cell *>> board, int level, int row, int col); //JBlock constructor
 		void Rotate(char direction, std::vector<std::vector<Cell *>> board) override;
+		//Rotates the Jblock clockwise and counterclockwise according to the direction on the given board
 };
 
 #endif
