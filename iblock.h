c38bc341baee598a6148a43766761278ea347a7f
EdwinYi-JanYang <51840314+EdwinYi-JanYang@users.noreply.github.com>
2021-08-12 16:21:06 -0400

    Update iblock.h

--- iblock.h
+++ iblock.h
@@ -9,7 +9,7 @@
 class IBlock : public Block {
 	bool vertical; //True if the Iblock is vertical, false if horizontal
 	public:
-		IBlock(std::vector<std::vector<Cell *>> board, int level, int row, int col); //Iblock constructor
+		IBlock(std::vector<std::vector<Cell *>> board, int level, int row, int col); //IBlock constructor
 		void Rotate(char direction, std::vector<std::vector<Cell *>> board) override;	
 		//Rotates the Iblock clockwise and counterclockwise according to the direction on the given board
 };
